04705ad9375256217c452e1efeb29d52672b42fa
Juan <>
2022-04-06 12:09:37 -0500

    PRACTICA 4

---
+++ src/Operaciones/basicas.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Operaciones;
+
+/**
+ *
+ * @author user
+ */
+public class basicas {
+    public int Sumar(int num1, int num2){
+        return (num1+num2);
+    }
+    
+    public int Resta(int num1, int num2){
+        return(num1-num2);
+    }
+    
+}
